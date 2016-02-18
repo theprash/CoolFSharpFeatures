8b7f6e7e10f436dab4b35c9d5d0c7b7c09be9fca
theprash <theprash@gmail.com>
2016-02-18 09:13:26 +0000

    Update 4_Currying.fsx

--- 4_Currying.fsx
+++ 4_Currying.fsx
@@ -3,11 +3,18 @@ let add a b = a + b
 // Partial application
 let addThree x = add 3 x
 
-// Currying
+// Currying (automatic partial application)
 let addThree' = add 3
 
 addThree' 1
 
+// Every function can be thought of as having one parameter and returning either the next function
+// to call or the final value.
+
+let f a b c d = a + b + c + d
+
+f 1 2 3 4         // <- how you normally write it
+((((f 1) 2) 3) 4) // <- an equivalent model
+
 // Convert this to a concise curried version:
-[1 .. 4]
-|> fun list -> List.map (fun x -> add 3 x) list
+[1 .. 4] |> fun list -> List.map (fun x -> add 3 x) list
