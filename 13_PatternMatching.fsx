ad212b47dcdb366163964f312fca7a938119e66f
theprash <theprash@gmail.com>
2016-02-19 09:09:02 +0000

    Reordered, split out into smaller examples and refactored fizzbuzz

--- 13_PatternMatching.fsx
+++ 13_PatternMatching.fsx
@@ -1,42 +1,69 @@
-// FizzBuzz : Print the numbers from 1 to 100,
-// but for multiples of three print “Fizz” instead of the number,
-// for the multiples of five print “Buzz”,
-// and for numbers which are multiples of both three and five print “FizzBuzz”.
+// Match on lists and their values
 
-let fb n =
-    match (n % 3), (n % 5) with
-    | 0, 0 -> "FizzBuzz" // Match on literal value
-    | 0, _ -> "Fizz"     // Match on wildcard `_`
-    | _, 0 -> "Buzz"
-    | _ -> string n
+match [ 1; 2; 3 ] with
+| [ _; _; 1 ] -> "Nope"
+| [ a; b; 3 ] -> "Yep. Oh, and the first two items are " + (string a) + " and " + (string b)
+  // a and b are new variables being bound here to any value in that list position.
+| _ -> "No matches"
 
-List.map fb [1..100]
+// Pattern matching is a way to take some input, and pick a path of execution that depends on its
+// shape or its value or parts of its shape or parts of its value, while also binding parts of its
+// value to names for further processing.
 
-// Match on lists
+// Match on empty list or the head of a list
 
-let input = [ [ "a"; "b"; "c" ]; [] ]
+match [ 1; 2; 3 ] with
+| [] -> "Empty"
+| head :: tail -> "Head is " + (string head) + ", and tail is " + (string tail)
 
-match input with
-| [ [ x; y; z ]; _ ]                -> (x, y, z) |> string
-| [ x; y ]                          -> y |> string
-| [ _; [] ]                         -> "second empty"
-| (x :: y :: _) as firstList :: _   -> (x, y, firstList) |> string
-| [ [ x; y ]; [] ]                  -> "two items in first list"
-| ( x :: _ ) :: _ when x.Length = 1 -> "first string is one character long"
-| _                                 -> "No matches"
+// Patterns can be nested.
+
+match [ [ 1; 2; 3 ]; [ 4; 5; 6] ] with // A list of lists.
+| [ a :: _ ; _ ] -> string a
+| _ -> "No matches"
+
+// Use a `when` clause with an arbitrary bool expression
+match [ 1; 2; 3 ] with
+| a :: _ when a > 1 -> "Match"
+| _ -> "No matches"
 
 // Match on records
 
 type MyCoolRecord = { name: string; age: int }
 
 match { name = "Gandalf"; age = 200 } with
-| { name = n; age = a } when a < 100 -> n.ToString() + " is less than 100 years old."
-| { name = n } -> n.ToString() + " is at least 100 years old."
+| { name = "Gandalf"; age = a } -> string a
+| _ -> "No matches"
 
 // Match on discriminated union cases
 
-type DU = Case1 | Case2 of string
+type DU =
+    | Case1
+    | Case2 of string
 
 match Case2 ":D" with
 | Case1 -> ":("
 | Case2 x -> x
+
+// Match on tuples
+
+match 5, "hi", [1; 2; 3] with // a 3-tuple
+| 0, _, _ -> "Nope"
+| _, _, (a :: _) -> string a
+| _ -> "No matches"
+
+
+// FizzBuzz : Print the numbers from 1 to 100,
+// but for multiples of three print “Fizz” instead of the number,
+// for the multiples of five print “Buzz”,
+// and for numbers which are multiples of both three and five print “FizzBuzz”.
+
+let fb input =
+    let dividesBy x = (input % x) = 0
+
+    match dividesBy 3, dividesBy 5 with
+    | false, false -> string input
+    | d3, d5 -> (if d3 then "Fizz" else "") +
+                (if d5 then "Buzz" else "")
+
+List.map fb [1..100]
